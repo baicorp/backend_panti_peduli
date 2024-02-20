86507189695769c19e7fd01d378e84fc39f8cf43
baicorp <bagusatoki@gmail.com>
2024-02-20 23:48:12 +0700

    1

--- database.js
+++ database.js
@@ -1,13 +1,16 @@
 import mysql from "mysql";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
-const db = mysql.createConnection(process.env.DB_URL || {
+// const db = mysql.createConnection(process.env.DB_URL);
+const db = mysql.createConnection(
+  process.env.DB_URL || {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
-});
+    database: process.env.DB_DATABASE,
+  }
+);
 
 export default db;

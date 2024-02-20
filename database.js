6d27c942d41162c7da3549679d1bc72ff2d7cffa
baicorp <bagusatoki@gmail.com>
2024-02-21 00:12:28 +0700

    3

--- database.js
+++ database.js
@@ -1,33 +1,14 @@
-// import mysql from "mysql";
-// import dotenv from "dotenv";
+import mysql from "mysql";
+import dotenv from "dotenv";
 
-// dotenv.config();
+dotenv.config();
 
-// // const db = mysql.createConnection(process.env.DB_URL);
-// const db = mysql.createConnection(
-//   process.env.DB_URL || {
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_DATABASE,
-//   }
-// );
-
-// export default db;
-
-const pool = mysql.createPool({
+// const db = mysql.createConnection(process.env.DB_URL);
+const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
 });
 
-pool.getConnection((err, conn) => {
-  if (err) console.log(err);
-  console.log("Connected successfully");
-});
-
-module.exports = pool.promise();
+export default db;

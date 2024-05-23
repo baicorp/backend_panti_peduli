e626d07066c10f0184660e2c6b5b42d9ac5488bd
baicorp <bagusatoki@gmail.com>
2024-05-23 13:05:03 +0700

    new update

--- database.js
+++ database.js
@@ -6,6 +6,7 @@ dotenv.config();
 const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
+  port: 3307,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
 });
